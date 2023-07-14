5b23a05aaf9db45a32182a8306e158fd0632bfae
Jonathan Carr <rainercarr@gmail.com>
2023-07-14 16:08:39 -0500

    Update batch.bat

--- batch.bat
+++ batch.bat
@@ -1,4 +1,4 @@
 Echo Off
-python enterbanktransactions_in_process.py ./data/2020_05_PNC_8_Mint_Creek.csv HARRY harvey
-python enterbanktransactions_in_process.py ./data/2020_05_PNC_6_Mint_Creek.csv HARRY harvey
-python enterbanktransactions_in_process.py ./data/2020_05_SBH_1_Mint_Creek.csv HARRY harvey
+python enterbanktransactions_in_process.py ./data/2020_05_PNC_8_Mint_Creek.csv BWGOLDusername BWGOLDpassword
+python enterbanktransactions_in_process.py ./data/2020_05_PNC_6_Mint_Creek.csv BWGOLDusername BWGOLDpassword
+python enterbanktransactions_in_process.py ./data/2020_05_SBH_1_Mint_Creek.csv BWGOLDusername BWGOLDpassword
